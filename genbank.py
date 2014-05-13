28f5bc3b0515d8bbc352909bab53a6df623f3e95
wf <willfreyman@gmail.com>
2014-05-12 21:46:56 -0700

    functionality to save and reuse gb searches

--- genbank.py
+++ genbank.py
@@ -1,18 +1,22 @@
 #! /usr/bin/python
 
+
 import os
 import sys
 import gzip
+import pickle
 from Bio import Entrez
 from Bio import SeqIO
 from ftplib import FTP
 from util import Color
 
 
+
 class GenBankSetup():
     """
     Class responsible for downloading and indexing GenBank files.
     """
+
 
     @staticmethod
     def gettext(ftp, filename, outfile=None):
@@ -23,6 +27,7 @@ class GenBankSetup():
             outfile = sys.stdout
         ftp.retrlines("RETR " + filename, lambda s, w=outfile.write: w(s+"\n"))
 
+
     @staticmethod
     def getbinary(ftp, filename, outfile=None):
         """
@@ -31,6 +36,7 @@ class GenBankSetup():
         if outfile is None:
             outfile = sys.stdout
         ftp.retrbinary("RETR " + filename, outfile.write)
+
 
     @classmethod
     def download(cls, division_input):
@@ -73,6 +79,7 @@ class GenBankSetup():
             sys.exit(0)
         ftp.quit()
 
+
     @staticmethod
     def sqlite():
         """
@@ -89,14 +96,18 @@ class GenBankSetup():
 
 
 
+
 class GenBankSearch:
     """
     Class responsible for searching GenBank and managing lists of keys 
     to all sequences in ingroup and outgroup.
     """
 
+    ingroup = ''
     ingroup_keys = []
+    outgroup = ''
     outgroup_keys = []
+  
 
     def __init__(self, gb, ingroup, outgroup):
         """
@@ -104,9 +115,24 @@ class GenBankSearch:
         and outgroup clades.
         Finds lists of keys to SeqRecords for the ingroup and outgroup.
         """
-        keys = gb.keys()
+        self.ingroup = ingroup
+        self.outgroup = outgroup
         self.ingroup_keys = []
         self.outgroup_keys = []
+
+        # check to see if this ingroup and outgroup have already been found
+        if self.check_for_results(): 
+            self.read_file()
+        else:
+            self.search(gb)
+
+
+    def search(self, gb):
+        """
+        Perform search of all GB SeqRecords for ingroup/outgroup,
+        and save results of search to file.
+        """
+        keys = gb.keys()
         total = len(keys)
         i = 0
         for key in keys:
@@ -114,7 +140,7 @@ class GenBankSearch:
                 self.ingroup_keys.append(key)
             elif outgroup in gb[key].annotations['taxonomy']:
                 self.outgroup_keys.append(key)
-            self.print_search_status(i)
+            self.print_search_status(i, total)
             i += 1
             ## FOR TESTING ONLY
             #if len(ingroup_keys) == 50:  ##
@@ -125,9 +151,10 @@ class GenBankSearch:
             ## remove above
         sys.stdout.write("\n")
         sys.stdout.flush()
-        return ingroup_keys, outgroup_keys
-    
-    def print_search_status(self, i):
+        self.write_file()
+
+
+    def print_search_status(self, i, total):
         color = Color()
         sys.stdout.write('\r' + color.yellow + 'Ingroup sequences found: ' \
                           + color.red + str(len(self.ingroup_keys)) + color.yellow \
@@ -138,9 +165,39 @@ class GenBankSearch:
                         )
         sys.stdout.flush()
 
-    def write_keys_to_file():
-        return True
 
-    def read_keys_from_file():
-        return True
+    def write_file(self):
+        """
+        Saves results of GB search to file.
+        """
+        data = {"ingroup": self.ingroup, "outgroup": self.outgroup, "ingroup_keys": self.ingroup_keys, "outgroup_keys": self.outgroup.keys}
+        pickle.dump(data, open( "genbank/gb_search_results", "rb" ))
 
+
+    def read_file(self):
+        """
+        Loads results of GB search from file.
+        """
+        groups = pickle.load( open( "genbank/gb_search_results", "rb" ) )
+        self.ingroup = groups["ingroup"]
+        self.outgroup = groups["outgroup"]
+        self.ingroup_keys = groups["ingroup_keys"]
+        self.outgroup_keys = groups["outgroup_keys"]
+        print(color.yellow + 'This search was already performed. Loading previous results...' + color.done)
+        print(color.yellow + 'Ingroup sequences found: ' \
+              + color.red + str(len(self.ingroup_keys)) + color.yellow \
+              + '  Outgroup sequences found: ' + color.red \
+              + str(len(self.outgroup_keys)) + color.done \
+             )
+
+
+    def check_for_results(self):
+        """
+        Check to see if ingroup/outgroup sequences have already been found.
+        """
+        if not os.path.exists("genbank/gb_search_results"):
+            return False
+        else:
+            groups = pickle.load( open( "genbank/gb_search_results", "rb" ) )
+            if self.ingroup == groups["ingroup"] and self.outgroup == groups["outgroup"]:
+                return True
